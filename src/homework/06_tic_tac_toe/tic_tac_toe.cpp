fd0cc18bdea9c0f540ddc153f581877ea19d4f74
leoqode <angel.jimenez2@g.austincc.edu>
2022-10-30 05:36:55 +0000

    Tic Tac Toe Homework 6 - With TCases

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,73 @@
-//cpp
+#include "tic_tac_toe.h"
+
+
+void Tic_Tac_Toe::display_board()const
+{
+    for(int i = 0; i < 3; i++)
+    {
+    cout<<"|"<<string_pegs[i]<<"|";
+    }
+    cout<<endl;
+    cout<<"--|---|--";
+    cout<<endl;
+
+    for(int i = 3; i < 6; i++)
+    {
+    cout<<"|"<<string_pegs[i]<<"|";
+    }
+    cout<<endl;
+    cout<<"--|---|--";
+    cout<<endl;
+
+
+    for(int i = 6; i < 9; i++)
+    {
+    cout<<"|"<<string_pegs[i]<<"|";
+    }
+    cout<<endl;
+    cout<<"--|---|--";
+    cout<<endl;
+}
+
+
+bool Tic_Tac_Toe::check_board_full()
+{
+    bool isFull = true;
+    for(int i = 0; i < 9; i++)
+    {
+        if(string_pegs[i] == " ")
+        {
+            isFull = false;
+        }
+    }
+    return isFull;
+}
+
+void Tic_Tac_Toe::clear_board()
+{
+    for(int i = 0; i < 9; i++)
+    {
+        string_pegs[i] = " ";
+    }
+}
+void Tic_Tac_Toe::start_game(string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+void Tic_Tac_Toe::set_next_player()
+{
+    if(player == "X")
+    {
+        player = "O";
+    }
+    else if(player == "O")
+    {
+        player = "X";
+    }
+}
+void Tic_Tac_Toe::mark_board(int position)
+{
+    string_pegs[position-1] = player;
+    set_next_player();
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,49 @@
-//h
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef TIC_TAC_TOE_h
+#define TIC_TAC_TOE_h
+
+
+class Tic_Tac_Toe{
+
+    public:
+
+        
+
+        
+        bool game_over(){return check_board_full();}
+
+
+        void start_game(string first_player);
+
+        void mark_board(int position);
+
+        string get_player() const ;
+
+        void display_board() const;
+
+        
+
+
+
+
+    private:
+        bool check_board_full();
+
+
+        void set_next_player();
+
+        void clear_board();
+        
+        string player;
+
+        vector<string> string_pegs = {" "," "," "," "," "," "," "," "," "};
+
+        
+
+};
+
+#endif // ending the definition of tic tac toe 
