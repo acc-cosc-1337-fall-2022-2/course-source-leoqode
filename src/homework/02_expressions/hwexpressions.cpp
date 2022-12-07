f626a474c30b1024d9765c22adb517f280aaf4fb
leoqode <angel.jimenez2@g.austincc.edu>
2022-12-07 03:09:11 +0000

    HW 2 With Test Cases

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,12 +1,13 @@
-#include "expressions.h"
+#include "hwexpressions.h"
 
-//example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
+
+double get_sales_tax_amount(double meal_amount){
+	const double tax_rate = 0.0675;
+	return meal_amount * tax_rate;
 }
-
-//write function code here
+double get_tip_amount(double meal_amount, double tip_rate){
+	return meal_amount * tip_rate;
+}
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,9 +1,10 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
 
-
+using namespace std;
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
@@ -11,5 +12,25 @@ int main()
 {
 	
 
-	return 0;
+	double meal_amount,tip_rate,tip_amount,tax_amount,total;
+
+	cout<<"Meal Amount: ";
+	cin>>meal_amount;
+	cout<<"\n";
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Tip Rate: ";
+	cin>>tip_rate;
+	cout<<"\n";
+
+	tip_amount = get_tip_amount(meal_amount,tip_rate);
+
+	total = tip_amount + meal_amount + tax_amount;
+
+	cout<<"RECIEPT"<<"\n\n";
+	cout<<"Tip Amount: "<<tip_amount<<endl;
+	cout<<"Meal Amount: "<<meal_amount<<endl;
+	cout<<"Tax Amount: "<<tax_amount<<endl;
+	cout<<"Total: "<<total<<endl;
 }
